c005c819a50052fea4934054f326fbbc7434536f
Kongsea <Kongsea@gmail.com>
2017-07-19 15:01:54 +0800

    Add restore-model-two-ways.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TensorFlow-tips
 Recording useful tips using TensorFlow
+
+1. [Two methods to restore a model.](./restore-model-two-ways.py)
---
+++ restore-model-two-ways.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import tensorflow as tf
+
+# files under model path
+# checkpoint
+# model.ckpt-****
+# model.ckpt-****.meta
+
+model_path = 'path'
+
+# the first method
+# model definitions
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(model_path)
+    ckpt.model_checkpoint_path = os.path.join(model_path, ckpt.model_checkpoint_path)
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        print('Model restore successfully.')
+
+# the second method
+# model definitions
+with tf.Session() as sess:
+    saver = tf.train.import_meta_graph(os.path.join(model_path, 'model.ckpt-****.meta'))
+    saver.restore(sess, os.path.join(model_path, 'model.ckpt-****'))
+# use tf.trainable_variables() to get all variables in models
+all_vars = tf.trainable_variables()
+for av in all_vars:
+    print('{}: {}'.format(av.name, sess.run(av).shape))
