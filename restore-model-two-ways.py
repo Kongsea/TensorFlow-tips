6465281503c5bdc3e6f477d90427a8eebe6fb945
Kongsea <Kongsea@gmail.com>
2017-07-25 08:52:54 +0800

    Refine.

--- restore-model-two-ways.py
+++ restore-model-two-ways.py
@@ -14,18 +14,18 @@ model_path = 'path'
 # model definitions
 saver = tf.train.Saver()
 with tf.Session() as sess:
-    ckpt = tf.train.get_checkpoint_state(model_path)
-    ckpt.model_checkpoint_path = os.path.join(model_path, ckpt.model_checkpoint_path)
-    if ckpt and ckpt.model_checkpoint_path:
-        saver.restore(sess, ckpt.model_checkpoint_path)
-        print('Model restore successfully.')
+  ckpt = tf.train.get_checkpoint_state(model_path)
+  ckpt.model_checkpoint_path = os.path.join(model_path, ckpt.model_checkpoint_path)
+  if ckpt and ckpt.model_checkpoint_path:
+    saver.restore(sess, ckpt.model_checkpoint_path)
+    print('Model restored successfully.')
 
 # the second method
 # model definitions
 with tf.Session() as sess:
-    saver = tf.train.import_meta_graph(os.path.join(model_path, 'model.ckpt-****.meta'))
-    saver.restore(sess, os.path.join(model_path, 'model.ckpt-****'))
-# use tf.trainable_variables() to get all variables in models
+  saver = tf.train.import_meta_graph(os.path.join(model_path, 'model.ckpt-****.meta'))
+  saver.restore(sess, os.path.join(model_path, 'model.ckpt-****'))
+# use tf.trainable_variables() to get all variables in the model
 all_vars = tf.trainable_variables()
 for av in all_vars:
-    print('{}: {}'.format(av.name, sess.run(av).shape))
+  print('{}: {}'.format(av.name, sess.run(av).shape))
