6a0dafc0a878b5f4587dcb94f7ee0ecae7dd4330
Kongsea <Kongsea@gmail.com>
2017-09-16 14:27:45 +0800

    Add more methods.

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Recording useful tips using TensorFlow
 
 1. [Two methods to restore a model.](./restore-model-two-ways.py)
+
+   A more detailed blog can be found [here](http://www.yueye.org/2017/summary-of-save-and-restore-models-in-tensorflow.html).
+
 2. [Set GPU settings.](./set_gpu.py)
 3. [Operate element-wise on a tensor.](./operate_element_wise.py)
 4. [Check if element of a tensor in elements of another tensor.](./check_element_of_tensor_in_element_of_another_tensor.py)
--- restore-model-two-ways.py
+++ restore-model-two-ways.py
@@ -21,11 +21,21 @@ with tf.Session() as sess:
     print('Model restored successfully.')
 
 # the second method
-# model definitions
+# don't need model definitions
+# use import_meta_graph to import graph definition
 with tf.Session() as sess:
+  # use meta file, such as: model.ckpt-1000.meta
   saver = tf.train.import_meta_graph(os.path.join(model_path, 'model.ckpt-****.meta'))
+  # use ckpt file, such as: model.ckpt-1000 (file name may be: model.ckpt-1000.data-000...)
   saver.restore(sess, os.path.join(model_path, 'model.ckpt-****'))
+  # or use model path: folder_path, which contains 4 files:
+  #                                                checkpoint
+  #                                                model.ckpt-1000.index
+  #                                                model.ckpt-1000.meta
+  #                                                model.ckpt-1000.data-00000-of-...
+  saver.restore(sess, tf.train.latest_checkpoint('./'))
   # use tf.trainable_variables() to get all variables in the model
+  # or operate on the variables as you need
   all_vars = tf.trainable_variables()
   for av in all_vars:
     print('{}: {}'.format(av.name, sess.run(av).shape))
